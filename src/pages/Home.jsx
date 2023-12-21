2b89c3cb303ab4113e6f527f8eb63193f77fd635
auscode <harshittext@gmail.com>
2023-12-21 14:31:16 +0530

    added reverse display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   useEffect(() => {
     appwriteService.getPosts().then((posts) => {
       if (posts) {
-        setPosts(posts.documents);
+        setPosts(posts.documents.reverse());
       }
     });
   }, []);
