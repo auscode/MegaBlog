12297e84df149d68058f62e1dfc0db0523f9b01d
auscode <harshittext@gmail.com>
2023-12-22 19:04:43 +0530

    home loading fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,25 @@
 import { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
+// import ClipLoader from "react-spinners/ClipLoader";
 
 function Home() {
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
-    appwriteService.getPosts().then((fetchedPosts) => {
-      if (fetchedPosts) {
-        setPosts(fetchedPosts.documents.reverse());
-        setIsLoading(false); 
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents.reverse());
       }
+      setLoading(false);
     });
   }, []);
 
-  if (isLoading) {
-    return (
-      <Container loading={isLoading}>
-        <div className="w-full py-8 mt-4 text-center">
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500">
-                Loading posts...
-              </h1>
-            </div>
-          </div>
-        </div>
-      </Container>
-    );
-  }
-
   if (posts.length === 0) {
     return (
-      <Container loading={false}>
-        <div className="w-full py-8 mt-4 text-center">
+      <div className="w-full py-8 mt-4 text-center">
+        <Container loading={loading}>
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
               <h1 className="text-2xl font-bold hover:text-gray-500">
@@ -42,14 +27,13 @@ function Home() {
               </h1>
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     );
   }
-
   return (
-    <Container loading={false}>
-      <div className="w-full py-8">
+    <div className="w-full py-8">
+      <Container loading={loading}>
         <div className="px-2 font-bold mb-2">Recent blog posts</div>
         <div className="flex flex-wrap">
           {posts.map((post) => (
@@ -58,8 +42,8 @@ function Home() {
             </div>
           ))}
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
